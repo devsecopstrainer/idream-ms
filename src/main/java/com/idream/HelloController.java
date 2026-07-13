4de41f118734c8162ba1e44e042dd53c4875e905
devsecopstrainer <devsecopstrainer@gmail.com>
2026-07-13 08:30:45 +0530

    first commit

---
+++ src/main/java/com/idream/HelloController.java
@@ -0,0 +1,19 @@
+package com.idream;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World from Spring Boot!";
+    }
+
+}
---
+++ src/main/java/com/idream/IdreamMsApplication.java
@@ -0,0 +1,13 @@
+package com.idream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdreamMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdreamMsApplication.class, args);
+	}
+
+}
