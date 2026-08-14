a7962144b13f3ae31a009a0d3c7d12cad61763cf
devsecopstrainer <devsecopstrainer@gmail.com>
2026-08-14 06:26:13 +0530

    Logger added

--- src/main/java/com/idream/HelloController.java
+++ src/main/java/com/idream/HelloController.java
@@ -1,14 +1,20 @@
 package com.idream;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/")
     public String home() {
-        return "Welcome to Spring Boot!";
+    	logger.debug("Entering hello() method");
+    	logger.info("Home endpoint accessed - serving welcome page");
+        return "<h1>Welcome to Spring Boot! App running successfully.</h1>";
     }
 
     @GetMapping("/hello")
